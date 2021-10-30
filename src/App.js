cc72be553b5cb12f96ef742ae72ac6727537b9fa
Sohail Haider <sohailh343@gmail.com>
2021-10-30 22:27:11 +0200

    antd comment removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import BingoHomePage from "./pages/BingoHomePage";
-// import 'antd/dist/antd.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <div className="container">
