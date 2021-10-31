03cb240a7de0b8ee3a337ea4213bb57efcd7cece
Sohail Haider <sohailh343@gmail.com>
2021-10-31 18:08:46 +0100

    confetti effect added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
       </h5>
       <Alert variant={"warning"}>
         <i>
-          Just one assumptions, for proper multiplayer game needed a server
+          Just one assumptions, for proper multiplayer game I needed a server
           where I could create a game session but here just for demonstration of
           reactjs skills I've limitized the implementation to just frontend.{" "}
         </i>
